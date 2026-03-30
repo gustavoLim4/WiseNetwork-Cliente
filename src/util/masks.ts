a87e0ce644ac11b76a9929eb4160f87a1a8ff76e
gustavoLim4 <gustavolll15567@gmail.com>
2026-03-30 17:56:12 -0300

    Mask adcionada

--- src/util/masks.ts
+++ src/util/masks.ts
@@ -1,20 +1,22 @@
 
 export const maskNome = (value: string) => {
-  return value
-    .replace(/[0-9]/g, '') 
-    .replace(/[^a-zA-ZÀ-ÿ\s]/g, ''); 
+    return value
+        .replace(/[0-9]/g, '')
+        .replace(/[^a-zA-ZÀ-ÿ\s]/g, '');
 };
 
 export const maskTelefone = (value: string) => {
-  const numbers = value.replace(/\D/g, '');
+    let numbers = value.replace(/\D/g, '');
 
-  if (numbers.length <= 10) {
+    numbers = numbers.slice(0, 11);
+
+    if (numbers.length <= 10) {
+        return numbers
+            .replace(/(\d{2})(\d)/, '($1) $2')
+            .replace(/(\d{4})(\d)/, '$1-$2');
+    }
+
     return numbers
-      .replace(/(\d{2})(\d)/, '($1) $2')
-      .replace(/(\d{4})(\d)/, '$1-$2');
-  }
-
-  return numbers
-    .replace(/(\d{2})(\d)/, '($1) $2')
-    .replace(/(\d{5})(\d)/, '$1-$2');
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        .replace(/(\d{5})(\d)/, '$1-$2');
 };
